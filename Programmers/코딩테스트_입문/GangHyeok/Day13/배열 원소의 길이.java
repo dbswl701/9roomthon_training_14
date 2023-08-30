8aec2aacae82ac5dbd2f464bfa5275ddcf75d3de
GangHyeok Lee <rkdgur5381@gmail.com>
2023-08-30 15:49:31 +0900

    프로그래머스: 배열 원소의 길이

---
+++ Programmers/코딩테스트_입문/GangHyeok/Day13/배열 원소의 길이.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = {};
+        
+        answer = new int[strlist.length];
+        
+        for(int i=0;i<strlist.length;i++){
+            answer[i] = strlist[i].length();
+        }
+        
+        return answer;
+    }
+}
