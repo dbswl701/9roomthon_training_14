a703199de835e22503c92e13ea9e89b6d777ef21
GangHyeok Lee <rkdgur5381@gmail.com>
2023-08-30 15:50:35 +0900

    프로그래머스: 다음에 올 숫자

---
+++ Programmers/코딩테스트_입문/GangHyeok/Day25/다음에 올 숫자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        if(common[1] - common[0] == common[common.length-1] - common[common.length-2]){
+            answer = 2*common[common.length-1] - common[common.length-2];
+        }
+        else{
+            answer = common[common.length-1]*(common[common.length-1] / common[common.length-2]);
+        }
+        
+        return answer;
+    }
+}
