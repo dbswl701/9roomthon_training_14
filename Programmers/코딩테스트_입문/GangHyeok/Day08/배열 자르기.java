f1ff5e34e714c7188887b37003e0cec43488b261
GangHyeok Lee <rkdgur5381@gmail.com>
2023-08-30 14:35:27 +0900

    프로그래머스: 배열 자르기

---
+++ Programmers/코딩테스트_입문/GangHyeok/Day08/배열 자르기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1+1];
+        
+        for(int i = 0;i<num2 - num1+1;i++){
+            answer[i] = numbers[num1 + i];
+        }
+        
+        
+        return answer;
+    }
+}
